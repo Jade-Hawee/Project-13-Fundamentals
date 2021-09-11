3dcbf404901703bac199aee1c61a7177f8457239
APPLE Laptop <applelaptop@APPLEs-MacBook-Pro.local>
2021-09-11 14:48:57 -0700

    first commit

---
+++ Linux/Roulette_Dealer_Finder_By_Time.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# find the roullette dealer on a given data ($1) at a given time ($2)
+# the script will return two name, the first being the dealer working the AM shift,
+# the second being the dealer working the PM shift
+grep -i $2 $1* | awk '{print $5" "$6}'
